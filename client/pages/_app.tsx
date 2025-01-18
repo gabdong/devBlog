993b2ec69a358b732e33f43ae6472f28c7ee9a9f
gabdong <hwan970104@gmail.com>
2025-01-18 20:59:54 +0900

    Fix: meta 태그 오타 수정

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -40,7 +40,7 @@ function App({ Component, ...rest }: AppProps): JSX.Element {
       <Head>
         <meta
           name="viewport"
-          content="initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;"
+          content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
         />
         <title>Gabdong</title>
       </Head>
